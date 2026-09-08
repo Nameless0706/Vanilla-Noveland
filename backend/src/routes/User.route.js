83b9dc0b1b77287aa856b9df5c34b33bb4700fa1
Thang Huynh <thanghuynh0706@gmail.com>
2026-09-09 03:30:17 +0700

    fml

--- backend/src/routes/User.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const router = express.Router();
-
-import userController from '../controllers/User.controller.js';
-import verifyJWT from '../middlewares/VerifyJWT.middleware.js';
-router.get('/all', verifyJWT, userController.getAllUsers); // /user/
-
-export default router;
